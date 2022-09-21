dfd74dd945a65373ba40f8acbaf349f4ef117785
Prince Codes <princecodes247@gmail.com>
2022-09-21 22:20:30 +0100

    update read fs

--- fs/read.go
+++ fs/read.go
@@ -20,8 +20,20 @@ func Read(path string) ([]byte, error) {
 	}
 	defer file.Close()
 	
+
+	// loop through the file and read it into a byte array
+
+	var bytes []byte
 	buffer := make([]byte, 4 * 1024)
-	
+	for {
+		n, err := file.Read(buffer)
+		if err != nil {
+			break
+		}
+		bytes = append(bytes, buffer[:n]...)
+	}
+
+	return bytes, err
 }
 
 
